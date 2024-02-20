7f6c6c410c37d48a9409bff287c1d0e08b0d3e60
KimberleyRaymond <143545353+KimberleyRaymond@users.noreply.github.com>
2024-02-20 15:56:40 -0500

    Add files via upload

---
+++ Python3/PE3_3.py
@@ -0,0 +1,5 @@
+#Calculate the BMI. Save the code as PE3_3.py.
+#a) Prompt and request the input for height(cm) and weight(kg) from the console.
+#b) Compute the body mass index and print it in three decimal places.
+bill=float(input("Enter the height :$"))
+bill=float(input("Enter the weight18 :$"))
